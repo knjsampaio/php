93784416632e70ae1f8cd1b3dfbf81491ac9c500
KEYVE NILDO JORDAO SAMPAIO <keyve.njsampaio@senacsp.edu.br>
2022-06-28 08:49:30 -0300

    testes php

---
+++ funcoes.php
@@ -0,0 +1,7 @@
+<?php
+// UDF - user defined functions
+// funções com parametros com passagem de valor ou referência
+function somar($valor1=60, $valor2=55, $valor3=3){
+    return $valor1 + $valor2 + $valor3;
+}
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once('funcoes.php'); 
+// comandos alternativos para linkar um arquivo: include 'funcoes.php , require()
+$idade = 16;
+echo "<h1>Fala Zé idade $idade</h1>";
+echo somar();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
