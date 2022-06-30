d7c5895c4ec5b694fe7fbfd95e87896aef8a528a
Keyve Sampaio <keyve10@live.com>
2022-06-30 02:02:44 -0300

    att

--- exercicio1.php
+++ exercicio1.php
@@ -14,18 +14,23 @@
     <title>Document</title>
 </head>
 <body>
-<form action="#" method="get">
+<form action="exercicio1.php" method="GET">
        Capital emprestado: <input type="text" name="capital"><br>
        Taxa de Juros: <input type="text" name="taxa"><br>
        Parcelas: <input type="text" name="parcela"><br>
         <button type="submit" name="btn">Enviar</button>
         <p>
             <?php
+
+            $capital = $_GET['capital'];
+            $taxa = $_GET['taxa'];
+            $parcela = $_GET['parcela'];
+            
             function calcularMontante($capital, $taxa, $parcela){
-                $montante = $_GET["capital"] * $_GET["taxa"] * $_GET["periodo"];
-                return $montante;
+                $resultado = $capital * $taxa * $periodo;
+                return $resultado;
             }
-            echo "O total é: " .$montante;
+        
             ?>
         </p>
     </form>
